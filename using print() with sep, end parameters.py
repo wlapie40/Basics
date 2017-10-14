7936fe59897961860fe1e5d6bd150ddd1983ae8a
wlapie40 <32576921+wlapie40@users.noreply.github.com>
2017-10-14 19:07:55 +0200

    Initial commit

---
+++ using print() with sep, end parameters.py
@@ -0,0 +1,10 @@
+a=[1,2,3,4,5,6]
+print("""It's our list: """,end='')
+print(a)
+
+print('With out separator: ',end='')
+print('Alice','Bob','Jastine','Rawler')
+
+print('End know with separator: ',end='')
+print('Alice','Bob','Jastine','Rawler',sep=',')
+
